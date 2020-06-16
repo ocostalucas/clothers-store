56fda20c5f936814463823b83d162f4306fc3170
ocostalucas <olucascosta98@gmail.com>
2020-06-16 15:24:10 -0300

    added product list

--- lib/tiles/category_tile.dart
+++ lib/tiles/category_tile.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:virtual_store/screens/product_screen.dart';
 
 class CategoryTile extends StatelessWidget {
   final DocumentSnapshot snapshot;
@@ -16,7 +17,11 @@ class CategoryTile extends StatelessWidget {
       ),
       title: Text(snapshot.data["title"]),
       trailing: Icon(Icons.keyboard_arrow_right),
-      onTap: (){},
+      onTap: (){
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context)=>ProductScreen(snapshot))
+        );
+      },
     );
   }
 }
