29b0bd22880ecaf8d5a9da5115e36b23f4d3f06a
ocostalucas <olucascosta98@gmail.com>
2020-07-03 11:10:18 -0300

    Add cart model

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:virtual_store/models/cart_model.dart';
 import 'package:virtual_store/models/user_model.dart';
 import 'package:virtual_store/screens/home_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
-    model: UserModel(), 
-    child: MaterialApp(
-      title: "Flutter's Clothing",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Color.fromARGB(255, 4, 125, 141),
+      model: UserModel(),
+      child: ScopedModelDescendant<UserModel>(
+          builder: (context, child, model){
+            return ScopedModel<CartModel>(
+              model: CartModel(model),
+              child: MaterialApp(
+                  title: "Flutter's Clothing",
+                  theme: ThemeData(
+                      primarySwatch: Colors.blue,
+                      primaryColor: Color.fromARGB(255, 4, 125, 141)
+                  ),
+                  debugShowCheckedModeBanner: false,
+                  home: HomeScreen()
+              ),
+            );
+          }
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    )
     );
   }
 }
---
+++ lib/models/cart_model.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:virtual_store/datas/cart_product.dart';
+import 'package:virtual_store/models/user_model.dart';
+
+class CartModel extends Model{
+  UserModel user;
+
+  List<CartProduct> products = [];
+
+  CartModel(this.user);
+
+
+  void addCartItem(CartProduct cartProduct){
+    products.add(cartProduct);
+
+    Firestore.instance.collection("users").document(user.firebaseUser.uid)
+      .collection("cart").add(cartProduct.toMap()).then((doc){
+        cartProduct.cid = doc.documentID;
+      });
+      notifyListeners();
+  }
+
+  void removeCartItem(CartProduct cartProduct){
+    Firestore.instance.collection("users").document(user.firebaseUser.uid)
+      .collection("cart").document(cartProduct.cid).delete();
+
+    products.remove(cartProduct);
+
+    notifyListeners();
+  }
+
+}
