94e79d7dff07d465bf1b58a4bb6f668073a1bbf8
ocostalucas <olucascosta98@gmail.com>
2020-07-05 21:44:41 -0300

    Add item in cart

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -4,34 +4,83 @@ import 'package:virtual_store/datas/cart_product.dart';
 import 'package:virtual_store/models/user_model.dart';
 import 'package:flutter/material.dart';
 
-class CartModel extends Model{
+class CartModel extends Model {
   UserModel user;
 
   List<CartProduct> products = [];
 
-  CartModel(this.user);
+  bool isLoading = false;
 
-  static CartModel of(BuildContext context) =>
-    ScopedModel.of<CartModel>(context);
-
-
-  void addCartItem(CartProduct cartProduct){
-    products.add(cartProduct);
-
-    Firestore.instance.collection("users").document(user.firebaseUser.uid)
-      .collection("cart").add(cartProduct.toMap()).then((doc){
-        cartProduct.cid = doc.documentID;
-      });
-      notifyListeners();
+  CartModel(this.user) {
+    if (user.isLoggedIn()) _loadCartItems();
   }
 
-  void removeCartItem(CartProduct cartProduct){
-    Firestore.instance.collection("users").document(user.firebaseUser.uid)
-      .collection("cart").document(cartProduct.cid).delete();
+  static CartModel of(BuildContext context) =>
+      ScopedModel.of<CartModel>(context);
+
+  void addCartItem(CartProduct cartProduct) {
+    products.add(cartProduct);
+
+    Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .add(cartProduct.toMap())
+        .then((doc) {
+      cartProduct.cid = doc.documentID;
+    });
+    notifyListeners();
+  }
+
+  void removeCartItem(CartProduct cartProduct) {
+    Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .document(cartProduct.cid)
+        .delete();
 
     products.remove(cartProduct);
 
     notifyListeners();
   }
 
+  void decProduct(CartProduct cartProduct) {
+    cartProduct.quantity--;
+
+    Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .document(cartProduct.cid)
+        .updateData(cartProduct.toMap());
+
+    notifyListeners();
+  }
+
+  void incProduct(CartProduct cartProduct) {
+    cartProduct.quantity++;
+
+    Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .document(cartProduct.cid)
+        .updateData(cartProduct.toMap());
+
+    notifyListeners();
+  }
+
+  void _loadCartItems() async {
+    QuerySnapshot query = await Firestore.instance
+        .collection("users")
+        .document(user.firebaseUser.uid)
+        .collection("cart")
+        .getDocuments();
+
+    products =
+        query.documents.map((doc) => CartProduct.fromDocument(doc)).toList();
+
+    notifyListeners();
+  }
 }
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:virtual_store/models/cart_model.dart';
+import 'package:virtual_store/models/user_model.dart';
+import 'package:virtual_store/screens/login_screen.dart';
+import 'package:virtual_store/tiles/cart_tile.dart';
 
 class CartScreen extends StatelessWidget {
   @override
@@ -24,6 +27,66 @@ class CartScreen extends StatelessWidget {
           )
         ]
       ),
+      body: ScopedModelDescendant<CartModel>(
+        builder: (context, child, model){
+          if(model.isLoading && UserModel.of(context).isLoggedIn()){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if(!UserModel.of(context).isLoggedIn()){
+            return Container(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.remove_shopping_cart,
+                    size: 80.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  SizedBox(
+                    height:16.0
+                  ),
+                  Text("Faça o login para adicionar produtos!",
+                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 16.0),
+                  RaisedButton(
+                    child: Text("Entrar", style: TextStyle(fontSize: 18.0)),
+                    textColor: Colors.white,
+                    color: Theme.of(context).primaryColor,
+                    onPressed: (){
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context)=> LoginScreen())
+                      );
+                    })
+                ],
+              ),
+            );
+          } else if(model.products == null || model.products.length == 0){
+            return Center(
+              child: Text("Nenhum produto no carrinho!", 
+              style: TextStyle(fontSize: 20.0,
+                fontWeight: FontWeight.bold
+              ),
+              textAlign: TextAlign.center,
+            )
+            );
+          } else{
+            return ListView(
+              children: <Widget>[
+                Column(
+                  children: model.products.map(
+                    (product){
+                    return CartTile(product);
+                  }).toList(),
+                )
+              ],
+            );
+          }
+        } ,
+        ),
     );
   }
 }
